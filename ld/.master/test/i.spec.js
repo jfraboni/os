34cef32c35bfb31f06afc571f6f945fb01533daf
John Fraboni <johnfraboni@hotmail.com>
2016-01-25 02:00:03 +0000

    * add reject, partition

--- ld/.master/test/i.spec.js
+++ ld/.master/test/i.spec.js
@@ -5,45 +5,6 @@ var
     customers = require('./fixtures/customers.json');
 
 describe('lodown', function() {
-    describe('each', function() {
-        it('should iterate an Array, applying action to each element, index of the element, and the collection', function() {
-            var action = sinon.spy();
-            lodown.each(customers, action);
-            expect(action.callCount).to.equal(customers.length);
-            customers.forEach(function(customer, index){
-               expect(action.calledWith(customer, index, customers)).to.be.true;
-            });
-        });
-   
-        it('should iterate an Object, applying action for each value, key of value, and Object', function() {
-            var action = sinon.spy();
-            var customer = customers[0];
-            lodown.each(customer, action);
-            expect(action.callCount).to.equal(Object.keys(customer).length);
-            for(var key in customer) {
-              expect(action.calledWith(customer[key], key, customer)).to.be.true;
-            }
-        });
-    });
-    
-    describe('filter', function() {
-        it('filter should filter Array items based on test provided', function () {
-            expect(lodown.filter([1, 2, 3], function(value) { return value > 2; } )).to.eql([3]);
-        });
-        it('filter should filter Object values based on test provided', function() {
-            expect(lodown.filter({one: 'one', two: 'two', three: 'three'}, function(value) { return value[0] === 't' })).to.eql(['two', 'three']);
-        });
-    });
-    
-    describe('map', function() {
-        it('map should create an Array of transformations for each value in an Array', function () {
-            expect(lodown.map([1, 2, 3], function(value) { return value + 1; } )).to.eql([2, 3, 4]);
-        });
-        it('map should create an Array of transformations for each value in an Object', function() {
-            expect(lodown.map({one: 'one', two: 'two', three: 'three'}, function(value) { return value.toUpperCase(); })).to.eql(['ONE', 'TWO', 'THREE']);
-        });
-    });
-    
     describe("identity", function() {
         it('identity should return the value passed to it, including types Number, String, Boolean, Array, Object, Function', function(){
             expect(lodown.identity(1)).to.equal(1);
@@ -62,6 +23,70 @@ describe('lodown', function() {
             expect(lodown.typeOf(null)).to.equal('null');
             expect(lodown.typeOf(new Date())).to.equal('date');
             expect(lodown.typeOf(function () { })).to.equal('function');
+        });
+    });
+    
+    describe('each', function() {
+        it('should iterate an Array, applying action to each element, index of the element, and the collection', function() {
+            var action = sinon.spy();
+            lodown.each(customers, action);
+            expect(action.callCount).to.equal(customers.length);
+            customers.forEach(function(customer, index){
+                expect(action.calledWith(customer, index, customers)).to.be.true;
+            });
+        });
+   
+        it('should iterate an Object, applying action for each value, key of value, and Object', function() {
+            var action = sinon.spy();
+            var customer = customers[0];
+            lodown.each(customer, action);
+            expect(action.callCount).to.equal(Object.keys(customer).length);
+            for(var key in customer) {
+                expect(action.calledWith(customer[key], key, customer)).to.be.true;
+            }
+        });
+    });
+    
+    describe('filter', function() {
+        it('filter should filter Array items based on test provided', function () {
+            expect(lodown.filter([1, 2, 3], function(value) { return value > 2; } )).to.eql([3]);
+        });
+        it('should call the test function with the value, index and Array', function(){
+            var test = sinon.spy();
+            lodown.filter(customers, test);
+            customers.forEach(function(customer, index){
+                expect(test.calledWith(customer, index, customers)).to.be.true;
+            });
+        });
+        it('filter should filter Object values based on test provided', function() {
+            expect(lodown.filter({one: 'one', two: 'two', three: 'three'}, function(value) { return value[0] === 't' })).to.eql(['two', 'three']);
+        });
+        it('should call the test function with the value, key and Object', function(){
+            var test = sinon.spy();
+            var customer = customers[0];
+            lodown.filter(customer, test);
+            for(var key in customer) {
+                expect(test.calledWith(customer[key], key, customer)).to.be.true;
+            }
+        });
+    });
+    
+    describe('reject', function() {
+        it('should reject elements in an Array wihtout side effects', function() {
+            var input = ["a", 1, "b", 2, "c", 4];
+            expect(lodown.reject(input, function(value, position, collection){
+                return typeof value === "string" || position < collection.length / 2;
+            })).to.eql([2,4]);
+            expect(input).to.eql(["a", 1, "b", 2, "c", 4]);
+        });
+    });
+    
+    describe('map', function() {
+        it('map should create an Array of transformations for each value in an Array', function () {
+            expect(lodown.map([1, 2, 3], function(value) { return value + 1; } )).to.eql([2, 3, 4]);
+        });
+        it('map should create an Array of transformations for each value in an Object', function() {
+            expect(lodown.map({one: 'one', two: 'two', three: 'three'}, function(value) { return value.toUpperCase(); })).to.eql(['ONE', 'TWO', 'THREE']);
         });
     });
     
@@ -96,5 +121,21 @@ describe('lodown', function() {
             expect(lodown.last({a:"b"}, 2)).to.eql([]);
         });
     });
-
+    
+    describe('partition', function() {
+        it('should take an Array and return Array containing two Array of passing and failing values, without side effects.', function() {
+            var input = ["a", 1, "b", 2, "c", 4];
+            expect(lodown.partition(input, function(value, index, array){
+                return typeof value === "string";
+            })).to.eql([["a","b","c"],[1,2,4]]);
+            expect(input).to.eql(["a", 1, "b", 2, "c", 4]);
+        });
+        it('should take an Object and return Array containing two Array of passing and failing values, without side effects.', function() {
+            var input = {one: 1, two: 2, three: 3, four: 4};
+            expect(lodown.partition(input, function(value, index, array){
+                return value < 3;
+            })).to.eql([[1, 2],[3, 4]]);
+            expect(input).to.eql({one: 1, two: 2, three: 3, four: 4});
+        });
+    });
 });
--- ld/index.js
+++ ld/index.js
@@ -10,10 +10,46 @@ function typeOf(value) {
         return 'array';
     } else if (value === null) {
         return 'null';
-    }
+    } else if (value instanceof Date) {
+        return 'date';
+    } 
     return typeof value;
 }
 module.exports.typeOf = typeOf;
+
+/**
+ * first: Returns the first or first n elements from the provided Array. 
+ * Provding a negative value for n will result in an empty Array. If n is 
+ * greater than the length of the Array, the full Array will be returned.
+ * 
+ * @param {Array} array The Array from which to draw the first value(s).
+ * @param {Number} n The number of beginning elements to be returned.
+ * 
+ * @return {String} A value or an Array of the first n values.
+ */
+function first(array, n) {
+    n = n || 1;
+    if (n < 1 || !Array.isArray(array)) return [];
+    return n > 1 ? array.slice(0, n) : array[0];
+}
+module.exports.first = first;
+
+/**
+ * last: Returns the last or last n elements from the provided Array. 
+ * Provding a negative value for n will result in an empty Array. If n is 
+ * greater than the length of the Array, the full Array will be returned.
+ * 
+ * @param {Array} array The Array from which to draw the last value(s).
+ * @param {Number} n The number of ending elements to be returned.
+ * 
+ * @return {String} A value or an Array of the first n values.
+ */
+function last(array, n) {
+    n = n || 1;
+    if (n < 1 || !Array.isArray(array)) return [];
+    return n > 1 ? array.slice(-n) : array[array.length - 1];
+}
+module.exports.last = last;
 
 /**
  * each: Designed to loop over a collection, Array or Object, and applies the action 
@@ -37,25 +73,6 @@ function each(collection, action) {
 module.exports.each = each;
 
 /**
- * Based a a test Function, filter is designed to collect values from a 
- * collection, Array or Object, and return those filtered values.
- * 
- * @param {Array or Object} collection The collection from which to filter.
- * @param {Function} test The Function that tests whether values should be 
- * inlcuded in the returned output. The test Function MUST return a Boolean.
- * 
- * @return {Array} An Array of the filtered values.
- */
-function filter(collection, test) {
-    var filtered = [];
-    each(collection, function(value, position, collection) {
-        if(test(value, position, collection)) filtered.push(value);
-    });
-    return filtered;
-}
-module.exports.filter = filter;
-
-/**
  * Takes a collection, and applies a tranformation function to each value in 
  * the collection, and returns an Array of the tranformations.
  * 
@@ -73,3 +90,63 @@ function map(collection, transform) {
     return mapped;
 }
 module.exports.map = map;
+
+/**
+ * fitler: Based a a test Function, filter is designed to collect values from 
+ * a collection, Array or Object, and return those filtered values.
+ * 
+ * @param {Array or Object} collection The collection from which to filter.
+ * @param {Function} test The Function that tests whether values should be 
+ * inlcuded in the returned output. The test Function MUST return a Boolean.
+ * The test Function is invoked with the value, the index or key, and the 
+ * collection.
+ * 
+ * @return {Array} An Array of the filtered values.
+ */
+function filter(collection, test) {
+    var filtered = [];
+    each(collection, function(value, position, collection) {
+        if(test(value, position, collection)) filtered.push(value);
+    });
+    return filtered;
+}
+module.exports.filter = filter;
+
+/**
+ * reject: Returns an Array of values from a collection, Array or Object
+ * that failed a test Function.
+ * 
+ * @param {Array or Object} collection The collection from which to reject.
+ * @param {Function} test The Function that tests whether values should be 
+ * rejected. If the test Function returns false, the value is added to the 
+ * rejected output. The test Function MUST return a Boolean. The test Function 
+ * is invoked with the value, the index or key, and the collection.
+ * 
+ * @return {Array} An Array of the rejected values.
+ */
+function reject(collection, test) {
+    return filter(collection, function (value, position, collection) {
+        return !test(value, position, collection);
+    });
+}
+module.exports.reject = reject;
+
+/**
+ * partition: Takes a collection and a test to apply to each value in the 
+ * collection, and returns an Array containing an Array of values that passed 
+ * the test and an Array of those that failed.
+ * 
+ * @param {Array or Object} collection The collection to partition.
+ * @param {Function} test The Function that defines how to partition, If the 
+ * test Function returns true, the value is added to the first Array, otherwise 
+ * the value is added to the second Array. The test Function MUST return a 
+ * Boolean. The test Function is invoked with the value, the index or key, and 
+ * the collection.
+ * 
+ * @return {Array} An Array containing an Array of values that passed 
+ * the test and an Array of those that failed.
+ */
+function partition(array, test) {
+    return [filter(array, test), reject(array, test)];
+}
+module.exports.partition = partition;
